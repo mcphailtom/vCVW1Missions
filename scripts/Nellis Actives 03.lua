602d3c2c7b85d35f942e4b0939888745f1d0525e
Tom McPhail <mcphailtom@github.com>
2024-09-02 22:23:49 +1000

    Fixed B2 issues

--- scripts/Nellis Actives 03.lua
+++
@@ -1,4 +0,0 @@
--- Flight Controller
-local nellisAB = AIRBASE:FindByName(AIRBASE.Nevada.Nellis)
-nellisAB:SetActiveRunwayLanding("03L")
-nellisAB:SetActiveRunwayTakeoff("03R")
--- scripts/Nellis Actives 21.lua
+++
@@ -1,4 +0,0 @@
--- Flight Controller
-local nellisAB = AIRBASE:FindByName(AIRBASE.Nevada.Nellis)
-nellisAB:SetActiveRunwayLanding("21L")
-nellisAB:SetActiveRunwayTakeoff("21R")
